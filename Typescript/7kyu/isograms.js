ba92eaef7b33aa812cc9f04d1949214519253977
Charlie <charlie.allen97@hotmail.com>
2024-06-04 15:51:49 +0100

    isograms update

--- Typescript/7kyu/isograms.js
+++ Typescript/7kyu/isograms.js
@@ -8,17 +8,22 @@ exports.isIsogram = void 0;
 // "Dermatoglyphics" -- > true
 // "aba" -- > false
 // "moOse" -- > false(ignore letter case)
+// submitted
+// export function isIsogram(str: string): boolean {
+//   str = str.toLowerCase();
+//   let temp: string = str;
+//   let ans: boolean = true;
+//   for (let i: number = 0; i < str.length; i++) {
+//     temp = temp.slice(1);
+//     if (temp.includes(str[i])) {
+//       ans = false;
+//     }
+//   }
+//   return ans;
+// }
+// update
 function isIsogram(str) {
-    str = str.toLowerCase();
-    var temp = str;
-    var ans = true;
-    for (var i = 0; i < str.length; i++) {
-        temp = temp.slice(1);
-        if (temp.includes(str[i])) {
-            ans = false;
-        }
-    }
-    return ans;
+    return (new Set(str.toLowerCase())).size === str.length;
 }
 exports.isIsogram = isIsogram;
 console.log(isIsogram("Dermatoglyphics"));
